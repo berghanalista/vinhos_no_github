b4926ca8a9c1df48eb0ec6e118b3af1d1c029250
Roozembergh <berghanalista@gmail.com>
2024-07-05 06:25:26 -0300

    Estrutura modificada
    Adicionando telas cadastro cliente e cadastro de produtos

---
+++ src/main/java/com/berghgustavoarianadayane/Configuration.java
@@ -0,0 +1,15 @@
+package com.berghgustavoarianadayane;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+/**
+ *
+ * @author berghlinux
+ */
+
+@ApplicationScoped
+@FacesConfig(version = FacesConfig.Version.JSF_2_3)
+public class Configuration {
+    
+}
---
+++ src/main/webapp/resources/js/layoutscript.js
@@ -0,0 +1,17 @@
+
+window.onload = function () {
+
+
+    const toggle = document.getElementById("v-toggle");
+    toggle.addEventListener("click", function () {
+
+        const sidebar = document.getElementsByClassName("js-sidebar")[0];
+        const content = document.getElementsByClassName("js-content")[0];
+
+        sidebar.classList.toggle("is-toggled");
+        content.classList.toggle("is-toggled");
+
+
+
+    });
+}
